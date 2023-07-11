72692f687799c4c3dba6ffd4033a57f30868c014
TuanAn <tuananthptcc10a5@gmail.com>
2023-07-12 01:22:44 +0700

    first commit

---
+++ test/dart_application_1_test.dart
@@ -0,0 +1,115 @@
+// import 'package:dart_application_1/dart_application_1.dart';
+// import 'package:test/test.dart';
+
+void main() {
+  //1. print statement
+  //print("hello!");
+  //print(3+2);
+  //print('3'+"2")
+
+
+
+  //2. variable
+  //a. contructor
+  // <datatype> <variablename> = value
+  // b. int type
+  // int a = 5;
+  // int b = 10;
+  // print("$a+$b = ${a+b}");
+  // print("$a-$b = ${a-b}");
+  // print("$a*$b = ${a*b}");
+  // print("$a/$b = ${a/b}");
+  //c. String type
+  // String a = "Hello, world!";
+  // print(a);
+  //d. Boolean type
+  // bool check = true;
+  // print(check);
+  // e. dynamic type
+  // dynamic a = 10.5;
+  // print(a);
+  // print(a.runtimeType);
+  // dynamic b = "Hello";
+  // print(b);
+  // print(b.runtimeType);
+  // dynamic c = 1;
+  // print(c);
+  // print(c.runtimeType);
+  // dynamic d = true;
+  // print(d.runtimeType);
+  // ham dynamic cho phep chua bat ky gia tri cua kieu nao nhung no co han che la khong su dung duoc cac props cua cac kieu do
+  // f. su dung cac props cua type
+  // int a =10;
+  // print(a.isOdd);
+  // print(a.isEven);
+  // String str1 = "Hello!";
+  // print(str1.length);
+  // print(str1.isEmpty);
+  // print(str1.hashCode);
+  // print(str1.isNotEmpty);
+  // print(str1.codeUnits);
+  // print(str1.runes);
+  // g . ghi de bien
+  // int a = 1;
+  // print(a);
+  // a = 2;
+  // print(a);
+  // a = a + 10;
+  // print(a);
+  // String str2 = "Hello";
+  // print(str2);
+  // str2 = str2 + " World!";
+  // print(str2);
+  // str2 = "${str2} Yoo";
+  // print(str2);
+  // khong the ghi de 2 kieu khac nhau nhung co the ghi de dynamic
+  // dynamic a = "Hello!";
+  // print(a);
+  // a = 10;
+  // print(a);
+
+
+
+  //3. Var , const and final
+  // khac nhau giua const va final la :
+  // const phai cu the , final thi co the duoc thuc thi trong qua trinh runtime
+  // var a = 100;
+  // print(a);
+  // const a1= DateTime.now(); // khong duoc
+  // final a2= DateTime.now(); // duoc
+  // print(a1);
+  // print(a2);
+
+  //4. Optional Variable/Nullable variable
+  // String? a;
+  // print(a);
+  // a = "oK";
+  // print(a.length);
+  // a = null;
+  // print(a?.length);
+
+  //5. Condition Statement
+  //a. If statement
+  // int age = 13;
+  // if(age > 13){
+  //   print("ok!");
+  // }else{
+  //   print("not ok!");
+  // }
+  // b. Ternary
+  // String a = "hello";
+  // String check = a.length>1 ? "You ok" : "You not ok!";
+  // print(check);
+  //c. Switch Statement
+  // int check = 4;
+  // switch(check){
+  //   case 1:
+  //   print('ok la 1!');
+  //   case 2:
+  //   print("Ok la 2!");
+  //   case 3:
+  //   print("ok la 3!");
+  //   default:
+  //   print("sai r ");
+  // }
+}
